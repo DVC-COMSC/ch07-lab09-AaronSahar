c0f8f43a5f3efe52ebd299ded82f9e3c0b7d32b8
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 20:27:46 -0700

    test

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-# ******************************
-# Make your Code
-# ******************************
+inputvalues = input()
+names = inputvalues.split()
+for i in range(len(names)):
+	names[i] = str(names[i]) 
+shortest = min(names, key=len)
+longest = max(names, key=len)
+print(shortest, longest)
