091a2e4beefd0d76b524171e2df686043c789924
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 20:34:42 -0700

    test

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ inputvalues = input()
 names = inputvalues.split()
 for i in range(len(names)):
 	names[i] = str(names[i]) 
-shortest = min(names, key=len)
-longest = max(names, key=len)
+shortest = names[0]
+longest = names[0]
+
+for i in range(len(names)):
+    if len(shortest) > len(names[i]):
+        shortest = names[i]
+    if len(shortest) == len(names[i]):
+        if shortest > names[i]:
+            shortest = names[i]
+    if len(longest) < len(names[i]):
+        longest = names[i]
+    if len(longest) == len(names[i]):
+        if longest > names[i]:
+            longest = names[i]
+
 print(shortest, longest)
